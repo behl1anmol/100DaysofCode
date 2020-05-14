c99343bb14353826f5a4240e6ffb9042d7a0501a
anmol behl <behl1anmol@gmail.com>
2020-05-14 16:25:50 +0530

    Day2_code1

---
+++ Day_2/first_set_bit.cpp
@@ -0,0 +1,36 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+unsigned int getFirstSetBit(int n){
+    
+    int j=0;
+    
+    if(n==0)
+        return 0;
+    
+    while(1){
+        if(n&1==1){
+            j++;break;
+        }
+        j+=1;
+        n=n>>1;
+    }
+    return j;
+    
+    
+}
+
+
+int main()
+{
+    int t;
+    cin>>t; 
+    while(t--)
+    {
+        int n;
+        cin>>n; 
+        printf("%u\n", getFirstSetBit(n));
+    }
+	return 0;
+}
