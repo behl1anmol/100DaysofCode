386d7fcc9cbf512977914e02165def0cfa96a62e
anmol behl <behl1anmol@gmail.com>
2020-05-24 20:05:41 +0530

    day 9 codes

--- Day_9/array_manipulation.py
+++ Day_9/array_manipulation.py
@@ -7,10 +7,10 @@ arr=[0]*(n+1)
 for i in range(m):
     a,b,k=[int(i) for i in input().split(" ")]
     arr[a]+=k
-    if((b+1)<=n): arr[b+1]-=k;
+    if((b+1)<=n): arr[b+1]-=k
 maximum=0 
 temp=0
 for i in arr:
-   temp=temp+i;
-   if(maximum<temp): maximum=temp;
+   temp=temp+i
+   if(maximum<temp): maximum=temp
 print(maximum)
