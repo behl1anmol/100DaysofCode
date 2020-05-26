34270856ef68158edef9c14a8feab3f9a843cff4
anmol behl <behl1anmol@gmail.com>
2020-05-26 20:57:31 +0530

    day 10 codes

---
+++ Day_10/sparse_array_dictionary.py
@@ -0,0 +1,21 @@
+"""
+sparse array hackerrank 
+O(n^2)
+"""
+n=int(input())
+d=dict()
+for i in range(n):
+    temp=input()
+    if temp in d.keys():
+        d[temp]+=1
+    else:
+        d[temp]=1    
+
+k=int(input())
+
+for i in range(k):
+    temp=input()
+    if temp not in d.keys():
+        print(0)
+    else:
+        print(d[temp])    
