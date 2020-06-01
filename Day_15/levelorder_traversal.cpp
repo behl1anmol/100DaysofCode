0caf450dd90f15b39bdb30aecda9116596909ce3
anmol behl <behl1anmol@gmail.com>
2020-06-01 08:35:49 +0530

    day 15 codes

--- Day_15/levelorder_traversal.cpp
+++ Day_15/levelorder_traversal.cpp
@@ -73,6 +73,32 @@ void level_order_traversal(Node *root){
 
 }
 
+void iterative_levelOrder(Node * root){
+        
+        if(root==NULL)
+            return;
+        
+        queue<Node*> q;
+
+        q.push(root); 
+
+        while(!q.empty()){
+            
+            Node *temp=q.front();
+            cout<<temp->data<<" ";
+            q.pop();
+
+            if(temp->left!=NULL){
+                q.push(temp->left);
+            }
+            if(temp->right!=NULL){
+                q.push(temp->right);
+            }
+            
+        }
+  	
+  
+	}
 int main(){
 
     int data,n;
