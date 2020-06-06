4ce89c7479e648798a98d12cda7911575943a382
anmol behl <behl1anmol@gmail.com>
2020-06-07 00:12:47 +0530

    adding comments to day 19 code<commiting late>

--- Day_19/amazon_interview_tree.cpp
+++ Day_19/amazon_interview_tree.cpp
@@ -5,13 +5,15 @@ S(n)=O(h)
 */
 #include<bits/stdc++.h>
 using namespace std;
+
+//class declaration for BST
 class Node{
 public:
     Node *left;
     int data;
     Node *right;
 
-
+    //constructor to initialize new
     Node(int data){
         left = NULL;
         right = NULL;
@@ -45,22 +47,31 @@ Node * Node:: insert(Node *root,int data){
 }
 
 bool is_leaf(Node *root){
-    
+
+    // if left and right child of the node is NULL return true else false
     return (root->left==NULL && root->right==NULL);
 }
 
-void delete_Node(Node* &root, int threshold,int k){
+void delete_Node(Node* &root, int threshold,int sum){
     
+    //if tree initially empty then return NULL
     if(root==NULL){
         return;
     }
 
-  
-    k=k+ (root->data);
-    delete_Node(root->left,threshold,k);
-    delete_Node(root->right,threshold,k);
+    //add the data of node to the sum so far
+    sum=sum+ (root->data);
 
-      if(is_leaf(root) && k<threshold){
+    //traverse towards the left subtree
+    delete_Node(root->left,threshold,sum);
+
+    //traverse towards the right subtree
+    delete_Node(root->right,threshold,sum);
+
+    /*if the node is leaf node and complete path 
+      is less than threshold then delete the node
+      from the tree*/
+      if(is_leaf(root) && sum<threshold){
         delete(root);
         root=NULL;
     }
@@ -70,11 +81,18 @@ void delete_Node(Node* &root, int threshold,int k){
 }
   void inOrder(Node *root) {
 
+        //if root initially empty return 
         if(root==NULL){
             return;
         }
+        
+        //traverse left subtree
         inOrder(root->left);
-        std::cout<<root->data<<" ";
+
+        //print data of node
+        cout<<root->data<<" ";
+
+        //traverse right subtree
         inOrder(root->right);
     }
 
@@ -91,11 +109,12 @@ int main(){
 
         root= root->insert(root,data);
     }
-    cout<<"\n Inorder traversal of new tree: ";
-
+ 
     int threshold,k=0;
     cin>>threshold;
 
+   cout<<"\n Inorder traversal of intital tree: ";
+   inOrder(root);
     delete_Node(root,threshold,k);
     
     cout<<"\n Inorder traversal of new tree: ";
