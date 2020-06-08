ef6565fbc644bce6000ea9fe7bded612681dbfdf
anmol behl <behl1anmol@gmail.com>
2020-06-08 20:47:19 +0530

    Day_15 code fixing

--- Day_15/levelorder_traversal.cpp
+++ Day_15/levelorder_traversal.cpp
@@ -39,39 +39,7 @@ Node * insert(Node *root,int data){
         
 }
 
-void _level_order_traversal(Node *root,queue<int> &q){
 
-    if(root==NULL)
-        return;
-
-
-    if(root->left!=NULL)
-        q.push(root->left->data);
-    if(root->right!=NULL)
-        q.push(root->right->data);
-
-    _level_order_traversal(root->left,q);
-    _level_order_traversal(root->right,q);
-
-    return;
-
-}
-void level_order_traversal(Node *root){
-    if(root==NULL)
-        return;
-
-    queue<int> q;
-
-    q.push(root->data);
-    _level_order_traversal(root,q);
-
-    while(!q.empty()){
-        cout<<q.front()<<" ";
-        q.pop();
-    }
-    return;
-
-}
 
 void iterative_levelOrder(Node * root){
         
@@ -114,6 +82,6 @@ int main(){
         root= insert(root,data);
     }
 
-    level_order_traversal(root);
+    iterative_levelOrder(root);
     return 0;
 }
