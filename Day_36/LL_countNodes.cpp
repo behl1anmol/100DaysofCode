9ec2bd6d6d5b2d91faabeda7fe780ffc0b7f2fd2
anmol behl <behl1anmol@gmail.com>
2020-07-15 23:59:42 +0530

    Day_36 codes <commit late>

---
+++ Day_36/LL_countNodes.cpp
@@ -0,0 +1,65 @@
+//GeeksforGeeks count nodes in LL T(N) = O(N)
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+int getCount(struct Node* head);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < n-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        cout << getCount(head) << endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+/* Link list node */
+/*
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+}; */
+
+// head : reference to head of linked list
+int getCount(struct Node* head){
+
+    Node *temp = head;
+    
+    int count=0;
+    
+    while(temp!=NULL){
+        count++;
+        temp = temp->next;
+    }
+    return count;
+
+}
